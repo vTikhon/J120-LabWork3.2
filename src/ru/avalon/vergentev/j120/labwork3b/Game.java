96912d95bfe918e5e85756acc34bed639550b8c4
vTikhon <tikhon.v@gmail.com>
2022-04-30 11:41:46 +0300

    find empty button

--- src/ru/avalon/vergentev/j120/labwork3b/Game.java
+++ src/ru/avalon/vergentev/j120/labwork3b/Game.java
@@ -3,10 +3,13 @@ package ru.avalon.vergentev.j120.labwork3b;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class Game extends JFrame implements MouseMotionListener {
     JButton[] button = new JButton[15];
     String titleOfButton;
+    Map<Integer, Integer> coordinates;
+    Map<Integer, Map<Integer, Integer>> mapButtons, mapNodes;
     int x, y, xEmpty, yEmpty, width, height;
 
     public Game () {
@@ -51,25 +54,75 @@ public class Game extends JFrame implements MouseMotionListener {
         int mousePositionX = ((int) getMousePosition().getX() / 100) * 100;
         int mousePositionY = ((int) getMousePosition().getY() / 100) * 100;
         buttonPushed.setLocation(mousePositionX, mousePositionY);
-        searchingForTheEmptyNode(buttonPushed);
-        System.out.println(xEmpty + ", " + yEmpty);
+        savingButtonNodesInCollection();
+        searchingForTheEmptyNode();
+//        lalala();
+//        System.out.println(xEmpty + ", " + yEmpty);
 
     }
 
-    public void searchingForTheEmptyNode (JButton buttonPushed) {
-        for (int n = 0; n < 4; n++) {
-            for (int m = 0; m < 4; m++) {
-                for (JButton i : button) {
-                    x = (i.getX()) / i.getWidth();
-                    y = (i.getY()) / i.getHeight();
-                    if (n != x && m != y) {
-                        xEmpty = n;
-                        yEmpty = m;
-                    }
-                }
+    public void savingButtonNodesInCollection () {
+        mapButtons = new HashMap<>();
+        int j = 1;
+        for (JButton i : button) {
+            coordinates = new HashMap<>();
+            x = (i.getX()) / i.getWidth();
+            y = (i.getY()) / i.getHeight();
+            coordinates.put(x, y);
+            mapButtons.put(j, coordinates);
+            j++;
+//            System.out.println(map.get(i));
+        }
+    }
+
+    public void searchingForTheEmptyNode () {
+        //формируем коллекцию узлов по аналогии с коллекцией кнопок
+        mapNodes = new HashMap<>();
+        int j = 1;
+        for (int n = 0; n < 4; n++) {             //проходим...
+            for (int m = 0; m < 4; m++) {         //...сетку 4х4
+                coordinates = new HashMap<>();
+                coordinates.put(m, n);            //создаём пару координат в аналогичную коллекцию, что координаты кнопки
+                mapNodes.put(j, coordinates);
+                j++;
+            }
+        }
+        //сравниваем коллекции кнопок и узлов
+        for (Integer i : mapNodes.keySet()) {
+            if (!(mapButtons.containsValue(mapNodes.get(i)))) {
+                System.out.println(mapNodes.get(i));
             }
         }
     }
+
+    public void lalala () {
+        for (Integer i : mapNodes.keySet()) {
+            System.out.println(i + " " + mapNodes.get(i));
+        }
+        System.out.println("--------------");
+        for (Integer i : mapButtons.keySet()) {
+            System.out.println(i + " " + mapButtons.get(i));
+        }
+        System.out.println("=============");
+    }
+
+
+//    public void searchingForTheEmptyNode (JButton buttonPushed) {
+//        for (int n = 0; n < 4; n++) {             //проходим...
+//            for (int m = 0; m < 4; m++) {         //...сетку 4х4
+//                coordinates = new HashMap<>();
+//                coordinates.put(n, m);            //создаём пару координат в аналогичную коллекцию, что координаты кнопки
+//                for (JButton i : button) {        //для каждого узла сетки 4х4 проходим по всему списку координат кнопок
+//                    if (map.get(i) == coordinates) {         //если координаты кнопки совпадают с узлом сетки 4х4, то это пустая ячейка
+//                        xEmpty = n;
+//                        yEmpty = m;
+//                    }
+//                }
+//                coordinates.clear();
+//            }
+//        }
+////        System.out.println(xEmpty + ", " + yEmpty);
+//    }
 
 
 
